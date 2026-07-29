ef9aed83561e03ad22bc734e416df59abd6b6950
svercoutere <stefaan.vercoutere@ugent.be>
2026-07-29 16:17:23 +0200

    updating prompts +  version sync with app-decide + adding 'en' prefLabels for display in HVT

---
+++ config/scheduled-job-controller/config.js
@@ -0,0 +1,8 @@
+export default {
+  jobPredicatesToCopy: [
+    'http://mu.semte.ch/vocabularies/ext/shapeForTargets',
+    'http://mu.semte.ch/vocabularies/ext/propertyPathForText',
+    'http://mu.semte.ch/vocabularies/ext/splitDecisions',
+    'http://mu.semte.ch/vocabularies/ext/graphForTargets',
+  ],
+};
